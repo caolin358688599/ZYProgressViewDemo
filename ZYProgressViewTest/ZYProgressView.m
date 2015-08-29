3897bbd6b3f9543f14a79273003562239fded5ee
wzpziyi1 <1204473090@qq.com>
2015-08-29 20:04:42 +0800

    first commit

---
+++ ZYProgressViewTest/ZYProgressView.m
@@ -0,0 +1,21 @@
+//
+//  ZYProgressView.m
+//  ZYProgressViewTest
+//
+//  Created by 王志盼 on 15/8/17.
+//  Copyright (c) 2015年 王志盼. All rights reserved.
+//
+
+#import "ZYProgressView.h"
+
+@implementation ZYProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
