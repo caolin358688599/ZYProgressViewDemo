9f86781e30918a3d66ec7546960cf1a63bbda519
wzpziyi1 <1204473090@qq.com>
2015-08-29 20:05:38 +0800

    updata

--- ZYProgressViewTest/ViewController.m
+++ ZYProgressViewTest/ViewController.m
@@ -7,21 +7,58 @@
 //
 
 #import "ViewController.h"
+#import "ZYProgressView.h"
 
-@interface ViewController ()
-
+@interface ViewController () <ZYProgressViewDataSource, ZYProgressViewDelegate>
+@property (nonatomic, weak) ZYProgressView *progressView;
+@property (nonatomic, strong) NSArray *titles;
 @end
 
 @implementation ViewController
 
+- (NSArray *)titles
+{
+    if (_titles == nil) {
+        _titles = @[@"菜鸟", @"码农", @"高级工程师", @"项目经理", @"CTO", @"迎娶白富美"];
+    }
+    return _titles;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    ZYProgressView *progressView = [[ZYProgressView alloc] init];
+    progressView.frame = CGRectMake(0, 100, self.view.frame.size.width, 170);
+    progressView.dataSource = self;
+    progressView.delegate = self;
+    //纯代码实现，这个方法可调，可不调。但是如果是通过xib创建，必须要调用此方法
+    [progressView reloadData];
+    self.progressView = progressView;
+    [self.view addSubview:progressView];
+    
+    //显示到当前进度，从1开始
+    self.progressView.currentProgress = 6;
+    
+    //中间跳过两个状态,注意，存放的值，要从1开始
+    self.progressView.items = @[@(3),@(4)];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark ----ZYProgressViewDataSource
+
+//告诉progressView,总共要显示多少个进度
+- (NSUInteger)numberOfProgressInProgressView
+{
+    return self.titles.count;
 }
 
+//告诉progressView，每个进度的title，索引从0开始
+- (NSString *)progressView:(ZYProgressView *)progressView titleAtIndex:(NSUInteger)index
+{
+    return self.titles[index];
+}
+
+#pragma mark ----ZYProgressViewDelegate
+- (UIColor *)highlightColorForCircleViewInProgressView:(ZYProgressView *)progressView
+{
+    return [UIColor yellowColor];
+}
 @end
